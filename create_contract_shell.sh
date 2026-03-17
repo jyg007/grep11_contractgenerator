a96fb41c9544a6a72403f309278b3f0fde471000
jeanyves-girard <jeanyves.girard@fr.ibm.com>
2026-03-17 10:06:54 +0100

    fix create_contract_shell.sh

--- create_contract_shell.sh
+++ create_contract_shell.sh
@@ -16,10 +16,10 @@ cp -rf ./cfg ./docker-compose
 cp -rf ./nginx ./docker-compose
 #terraform apply -auto-approve
 
-sed -e 's#${tpl.imagegrep11}#'$IMAGEGREP11"#" -e 's#${tpl.imagenginx}#'$IMAGENGINX"#" grep11-c16.yml.tftpl > ./docker-compose/docker-compose.yml
+sed -e 's#${tpl.imagegrep11}#'$IMAGEGREP11"#" -e 's#${tpl.imagenginx}#'$IMAGENGINX"#" grep11-c16.yml.tftpl > ./docker-compose/play.yml
 
 sed -e 's/<<-EOT/$(cat <<-EOT /' -e 's/^EOT/EOT\n)/' ./terraform.tfvars > ./o.$$ 
-for i in IMAGE SYSLOG REGISTRY MACHINE2 MACHINE2_DESCRIPTION MACHINE2_HKD_B24 HSMDOMAIN2 MACHINE1 MACHINE1_DESCRIPTION MACHINE1_HKD_B24 HSMDOMAIN1 SECRET_B24 MKVP HELLO HPCR_CERT
+for i in IMAGE SYSLOG REGISTRY MACHINE2 MACHINE2_DESCRIPTION MACHINE2_HKD_B24 HSMDOMAIN2 MACHINE1 MACHINE1_DESCRIPTION MACHINE1_HKD_B24 HSMDOMAIN1 SECRET_B24 MKVP HPCR_CERT
 do
   sed -i "s/^$i/export $i/" ./o.$$
 done
@@ -27,8 +27,8 @@ done
 . ./o.$$
 rm ./o.$$
 
-sed -e "s/HSMDOMAIN/$HSMDOMAIN1/" grep11server.tpl > srv/grep11server1.yaml
-sed -e "s/HSMDOMAIN/$HSMDOMAIN2/" grep11server.tpl > srv/grep11server2.yaml
+sed -e "s/HSMDOMAIN/$HSMDOMAIN1/" -e "s/EP11SERVERPORT/10876/" grep11server.tpl > srv/grep11server1.yaml
+sed -e "s/HSMDOMAIN/$HSMDOMAIN2/" -e "s/EP11SERVERPORT/11876/" grep11server.tpl > srv/grep11server2.yaml
 cp -rf ./srv ./docker-compose
 
 export COMPOSE=`tar -cz -C docker-compose/ . | base64 -w0`
@@ -45,14 +45,14 @@ CONTRACT_KEY=.ibm-hyper-protect-container-runtime-encrypt.crt
 envsubst < hpcr_contractkey.tpl > $CONTRACT_KEY
 
 PASSWORD=`openssl rand -base64 32`
+ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl pkeyutl -encrypt -pubin -inkey <(openssl x509 -in $CONTRACT_KEY -pubkey -noout) |  base64 -w0)"
 #ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl rsautl -encrypt -inkey $CONTRACT_KEY -certin | base64 -w0 )"
-ENCRYPTED_PASSWORD=$(printf "%s" "$PASSWORD" | openssl pkeyutl -encrypt -inkey "$CONTRACT_KEY" -certin -pkeyopt rsa_padding_mode:pkcs1 | base64 -w0)
 ENCRYPTED_WORKLOAD="$(echo -n "$PASSWORD" | base64 -d | openssl enc -aes-256-cbc -pbkdf2 -pass stdin -in "$WORKLOAD" | base64 -w0)"
 echo "workload: hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_WORKLOAD}" > grep11.yml
 
 PASSWORD=`openssl rand -base64 32`
 #ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl rsautl -encrypt -inkey $CONTRACT_KEY -certin | base64 -w0 )"
-ENCRYPTED_PASSWORD=$(printf "%s" "$PASSWORD" | openssl pkeyutl -encrypt -inkey "$CONTRACT_KEY" -certin -pkeyopt rsa_padding_mode:pkcs1 | base64 -w0)
+ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl pkeyutl -encrypt -pubin -inkey <(openssl x509 -in $CONTRACT_KEY -pubkey -noout) |  base64 -w0)"
 ENCRYPTED_ENV="$(echo -n "$PASSWORD" | base64 -d | openssl enc -aes-256-cbc -pbkdf2 -pass stdin -in "$ENV" | base64 -w0)"
 echo "env: hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_ENV}" >> grep11.yml
  
